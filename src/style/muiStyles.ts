772793c8090b9341f64515629efad66bb9d8a00c
Shaked Hayek <shaked6248@gmail.com>
2025-03-31 15:46:35 +0300

    Fix select field arrow side

--- src/style/muiStyles.ts
+++ src/style/muiStyles.ts
@@ -8,6 +8,15 @@ export const rtlStyle = {
     '& legend': {
         textAlign: 'right',
     },
+    // For select fields
+    '& .MuiSelect-icon': {
+        right: 'auto',
+        left: 1,
+    },
+    '& .MuiSelect-select': {
+        paddingLeft: 4, // Add padding to prevent text overlapping with the icon
+        paddingRight: '14px !important', // Reduce right padding
+    },
 };
 
 export const formStyle = {
