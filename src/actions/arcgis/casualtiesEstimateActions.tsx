6b43d597c2f61f9c75d0f506704276957767b70b
Shaked Hayek <shaked6248@gmail.com>
2025-05-13 16:51:40 +0300

    Send params in casualties GET

--- src/actions/arcgis/casualtiesEstimateActions.tsx
+++ src/actions/arcgis/casualtiesEstimateActions.tsx
@@ -3,9 +3,12 @@ import { ROUTES } from 'actions/routes';
 import { SERVER_IP } from 'consts/settings.const';
 
 export async function getCasualtiesEstimate(street: string, streetNumber: string, setCasualtiesEstimate: Dispatch<SetStateAction<string>>) {
-    const response = await fetch(`${SERVER_IP}${ROUTES.CASUALTIES_ESTIMATE}`, {
-        method: 'GET',
-    });
+    const response = await fetch(
+            `${SERVER_IP}${ROUTES.CASUALTIES_ESTIMATE}?street=${street}&number=${streetNumber}`, 
+            {
+                method: 'GET',
+            }
+    );
     if (!response.ok) {
         throw new Error('Failed to get casualties estimate');
     }
