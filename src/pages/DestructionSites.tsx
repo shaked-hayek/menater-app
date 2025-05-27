daae838b661ae5b1f7bcc60ecc85e65bafdeed5d
Shaked Hayek <shaked6248@gmail.com>
2025-05-27 16:43:40 +0300

    Clean estimate

--- src/pages/DestructionSites.tsx
+++ src/pages/DestructionSites.tsx
@@ -68,6 +68,7 @@ const DestructionSites = () => {
             setSelectedStreet(null);
             setSelectedNumber('');
             setCasualties('');
+            setCasualtiesEstimate('');
         }
     };
 
