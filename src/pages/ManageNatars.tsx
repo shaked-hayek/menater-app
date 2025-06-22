9b69b7c5332bea96d13984b3486afc1987b45d8d
Shaked Hayek <shaked6248@gmail.com>
2025-06-22 11:19:02 +0300

    Add scroll to natar table

--- src/pages/ManageNatars.tsx
+++ src/pages/ManageNatars.tsx
@@ -10,6 +10,7 @@ import {
   IconButton,
   Box,
   Paper,
+  TableContainer,
 } from '@mui/material';
 import { Edit, Delete } from '@mui/icons-material';
 import { useTranslation } from 'react-i18next';
@@ -96,41 +97,43 @@ const ManageNatars = () => {
                     bgcolor: tableBgColor,
                 }}
             >
-                <Table stickyHeader>
-                    <TableHead>
-                        <TableRow>
-                            {fields.map(field => (
-                                <TableCell key={field.key} align='right' sx={{ fontWeight: 'bold' }}>
-                                    {field.label}
-                                </TableCell>
-                            ))}
-                            <TableCell align='right' sx={{ fontWeight: 'bold' }}>{t('manageNatars.actions')}</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {natars.map(natar => (
-                            <TableRow key={natar.id}>
-                                {fields.map(({ key }) =>
-                                    <TableCell key={key} align='right'>
-                                        {typeof natar[key] === 'boolean'
-                                        ? t(natar[key] ? 'openNatar.exists' : 'openNatar.doesntExist')
-                                        : String(natar[key] ?? '')}
+                <TableContainer sx={{ maxHeight: '445px' }}>
+                    <Table stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                {fields.map(field => (
+                                    <TableCell key={field.key} align='right' sx={{ fontWeight: 'bold' }}>
+                                        {field.label}
                                     </TableCell>
-                                )}
-                                <TableCell align='right'>
-                                    <Box display='flex' gap={1} justifyContent='flex-end'>
-                                        <IconButton onClick={() => handleEdit(natar)}>
-                                            <Edit color='primary' />
-                                        </IconButton>
-                                        <IconButton onClick={() => handleDelete(natar)}>
-                                            <Delete color='error' />
-                                        </IconButton>
-                                    </Box>
-                                </TableCell>
+                                ))}
+                                <TableCell align='right' sx={{ fontWeight: 'bold' }}>{t('manageNatars.actions')}</TableCell>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
+                        </TableHead>
+                        <TableBody>
+                            {natars.map(natar => (
+                                <TableRow key={natar.id}>
+                                    {fields.map(({ key }) =>
+                                        <TableCell key={key} align='right'>
+                                            {typeof natar[key] === 'boolean'
+                                            ? t(natar[key] ? 'openNatar.exists' : 'openNatar.doesntExist')
+                                            : String(natar[key] ?? '')}
+                                        </TableCell>
+                                    )}
+                                    <TableCell align='right'>
+                                        <Box display='flex' gap={1} justifyContent='flex-end'>
+                                            <IconButton onClick={() => handleEdit(natar)}>
+                                                <Edit color='primary' />
+                                            </IconButton>
+                                            <IconButton onClick={() => handleDelete(natar)}>
+                                                <Delete color='error' />
+                                            </IconButton>
+                                        </Box>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </Paper>
 
             <ErrorPopup
