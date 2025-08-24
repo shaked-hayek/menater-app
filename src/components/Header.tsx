0d45ed056617563870b18494d987800aa4f519f6
Shaked Hayek <shaked6248@gmail.com>
2025-08-24 16:09:20 +0300

    Save summary when clicking home as well

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 import TimeSince from './atoms/Timer';
 import { MODE } from 'consts/mode.const';
 import { themeColor, trialColor, waitingOrange, successGreen, errorRed } from 'style/colors';
+import { useNavigate } from 'react-router';
+import { createEventSummaryAction } from 'actions/events/eventSummaryActions';
+import { errorHandler } from 'actions/errors/errorHandler';
+
 
 const Header = () => {
     const { t } = useTranslation();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
     const [ currentTimeString, setCurrentTimeString ] = useState('');
     const { arcgisAuth, earthquakeEvent, mode } = useSelector((state: RootState) => state.appState);
 
@@ -23,12 +29,23 @@ const Header = () => {
         return () => clearInterval(interval); // Cleanup
     }, []);
 
+    const onHomeRoute = async () => {
+        if (earthquakeEvent?.id) {
+            try {
+                await createEventSummaryAction(earthquakeEvent.id);
+            } catch (error) {
+                errorHandler(dispatch, t('destructionSites.errorMsgs.errorGettingRecommendation'), error);
+            }
+        }
+        navigate('/');
+    }
+
     return (
         <AppBar position='static'>
             <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Box sx={{ display: 'flex', gap: 2 }}>
                     <Typography variant='h6'>{t('header.title')}</Typography>
-                    <Button color='inherit' component={Link} to='/'>
+                    <Button color='inherit' onClick={onHomeRoute}>
                         {t('header.home')}
                     </Button>
                     <Typography
